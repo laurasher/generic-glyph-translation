7800142eab7d2a936385b08065b4a78844f9bc33
Laura Asher <laura.asher@jhuapl.edu>
2021-11-19 13:39:44 -0500

    Progress commit.

--- web/run.py
+++ web/run.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 def home():
 	return render_template("glyph_translate.html")
 
+## 9/28/21 Try out more, and varied, marks
+@app.route('/marks', methods=['GET'])
+def marks():
+	return render_template("extended_glyph_translate.html")
+
 @app.route('/spiral', methods=['GET'])
 def spiral():
 	return render_template("glyph_translate_spiral.html")
@@ -21,6 +26,10 @@ def spectro_lines():
 def spectro():
 	# return render_template("spectro.html")
 	return render_template("spectro_blobs.html")
+
+@app.route('/drawn_lines', methods=['GET'])
+def drawn_lines():
+	return render_template("drawn_lines_translate.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5050)
